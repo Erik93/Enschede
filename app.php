dcdc62b2bfa7d4fc36db590ed7008a0f9d288850
Erik93 <ecbroekhuis@gmail.com>
2013-08-04 13:30:01 +0200

    Updated
    Added: id:"GETs" zodat het gelinkt word in het javascript bestand.

--- app.php
+++ app.php
@@ -10,7 +10,7 @@
 
 
 <h1> Search for events in Enschede!</h1>
-<form method="GET" action="https://www.uitinenschede.nl/api/rest/locations?apikey=38459234y5wefcgejrkukju">
+<form method="GET" id="GETs" action="https://www.uitinenschede.nl/api/rest/locations?apikey=38459234y5wefcgejrkukju">
 <input name="tags" value="">
 <input type="submit" value="search">
 </form>
