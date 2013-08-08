107b08b135141311a1b6468f9afa01ebb9c6bb9e
Erik93 <ecbroekhuis@gmail.com>
2013-08-08 17:39:49 +0200

    php code toegevoegd
    Het bestand is nu klaar

--- app.php
+++ app.php
@@ -1,44 +1,40 @@
-<html>
-<head>
-<meta charset="utf-8">
-<title>Naamloos document</title>
-</head>
-
-<body>
+<?php
 
 
-
-<h1> All events in Enschede!</h1>
-
-<?php 
-$url = "http://www.uitinenschede.nl/api/rest/events?apikey=NpuORYzCTCDqYe4PBOCnEmTYI22F6wkK";
-$response = file_get_contents($url);
-$data = json_decode($response, true);
-
-$num = count($data['events']);
-#print_r($data['events'][1]);
-
-$i = 0;
-while ($i < $num)
-{
-  echo "Titel: " . $data['events'][$i]['title'] . "<br>";
-	echo "Url: " . "<a href='" . $data['events'][$i]['url'] . "'>Link</a>" . "<br>";
-	echo "Beschrijving: " . $data['events'][$i]['description'] . "<br>";
-	echo "Datum: " . $data['events'][$i]['from'] . "<br><br>";
+error_reporting(E_ALL);
+add_action("widgets_init", array('NielsDoornsWidget', 'register'));
+class NielsDoornsWidget {
+  function control(){
+    echo 'I am a control panel for NielsDoornsWidget';
+  }
+  function widget($args){
+    echo $args['before_widget'];
+    echo $args['before_title'] . 'Enschede Events' . $args['after_title'];
 	
+		$url = "http://www.uitinenschede.nl/api/rest/events?apikey=NpuORYzCTCDqYe4PBOCnEmTYI22F6wkK";
+		$response = file_get_contents($url);
+		$data = json_decode($response, true);
+
+		$num = count($data['events']);
+
+		$i = 0;
+		while ($i < 3)
+		{
+			echo "Titel: " . $data['events'][$i]['title'] . "<br>";
+			echo "Url: " . "<a href='" . $data['events'][$i]['url'] . "'>Link</a>" . "<br>";
+			echo "Beschrijving: " . $data['events'][$i]['description'] . "<br>";
+			echo "Datum: " . $data['events'][$i]['from'] . "<br><br>";
 	
-	
-	$i++;
+			$i++;
+		}
+  
+    echo $args['after_widget'];
+  }
+  function register(){
+    register_sidebar_widget('Enschede Events', array('NielsDoornsWidget', 'widget'));
+    register_widget_control('Enschede Events', array('NielsDoornsWidget', 'control'));
+  }
 }
 
 
-
 ?>
-
-
-
-
-
-
-</body>
-</html>
